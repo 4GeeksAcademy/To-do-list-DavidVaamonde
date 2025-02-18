0c97636ddcfc8b87be15c342f0cd6871cf65308c
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-02-18 16:07:44 +0000

    Hecha la lista de tareas

--- src/js/components/ToDoList.jsx
+++ src/js/components/ToDoList.jsx
@@ -1,28 +1,45 @@
 import React, {useState} from 'react'
-import {render} from 'react-dom'
 
 //importar fontawesome
 import "https://kit.fontawesome.com/072e5df971.js"
-
-//importar componentes
-import AgregarTarea from './AgregarTarea'
-import ListaTareas from './ListaTareas'
 
 //crear componente
 const ToDoList = () => {
 
     const [inputTarea, setInputTarea ] = useState('');
-    const [lista, setLista] = useState([]);
+    const [lista, setLista] = useState(["Haz la cama", "Comer cachopo", "Vacaciones de verano"]);
 
-    const handleKeyDown = ((e) => {
-            console.log("Tecla presionada: "+e.key);
-            if(e.key === 'Enter') {
-                setLista(lista.concat(inputTarea));
-                console.log("Has pulsado Enter!!");
-                console.log("Agregado: "+setLista(lista));
-                setInputTarea("");
-            }
-    });
+    //Manejando la funcion de la tecla Enter
+    const manejoKeyPress = (e) => {
+        console.log("Tecla pulsada: "+e.key);
+        if (e.key === 'Enter') {
+            console.log("Has pulsado Enter!!");
+            setLista([...lista, inputTarea]);
+            setInputTarea('');
+        }
+    }
+
+    //Funcion para eliminar tareas
+    const eliminarTarea = (index) => {
+        const nuevaLista = lista.filter((_, i) => i !== index);
+        setLista(nuevaLista);
+    }
+
+    //Funcion para generar la lista junto con eliminarTarea
+    const generarListaTareas= () => {
+        let elementos = [];
+        for (let i = 0; i < lista.length; i++) {
+            elementos.push(
+                <li key={i}>
+                    {lista[i]}
+                    <button onClick={() => eliminarTarea(i)}>
+                        <i className="fa-solid fa-eraser"></i>
+                    </button>
+                </li>
+            );
+        }
+        return elementos;
+    }
 
     return (
         <>
@@ -34,44 +51,12 @@ const ToDoList = () => {
                         type="text"  
                         onChange={(e) => setInputTarea(e.target.value)}
                         value={inputTarea}
-                        onKeyDown={ handleKeyDown }
+                        onKeyDown={ manejoKeyPress }
                         placeholder='Añade aquí una tarea... '
                          />
                     </li>
-                    {lista.map((item, index) => {
-                        <li>
-                            {item}{" "}
-                            <i 
-                                className="fa-solid fa-eraser" 
-                                onClick={() => setLista(
-                                    lista.filter(
-                                        (t, currentIndex) => 
-                                            index != currentIndex)
-                                            
-                                            )
-                                        }
-                            />
-                        </li>
-                    })}
-                        
+                    {generarListaTareas()}
                     
-                    
-                    <li>
-                        Hacer los deberes
-                        <i className="fa-solid fa-eraser" />
-                    </li>
-                    <li>
-                        Tender la ropa
-                        <i className="fa-solid fa-eraser" />
-                    </li>
-                    <li>
-                        Cocinar
-                        <i className="fa-solid fa-eraser" />
-                    </li>
-                    <li>
-                        Planchar
-                        <i className="fa-solid fa-eraser" />
-                    </li>
                 </ul>
                 <div>{lista.length} tareas</div>
             </div>
